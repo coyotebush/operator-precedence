2fc28b38830bcebadc5395c65676fe1ebabcfcc8
Corey Ford <coyotebush22@gmail.com>
2012-10-12 22:57:34 -0700

    factor out user input

--- precedencequiz.py
+++ precedencequiz.py
@@ -28,12 +28,23 @@ operators = [
     ( 1, "{} , {}",  lambda x, y: y),
     ]
 
-random.seed()
-
 x = [0, 1, 2, 3, 4, 5]
 
-done = False
-while not done:
+def get_answer(ans):
+  while True:
+    try:
+      user_ans = raw_input("=> ")
+      if user_ans[0] == "?":
+        print int(ans)
+        return
+      elif int(user_ans) == ans:
+        return
+    except (ValueError, IndexError):
+      pass
+
+random.seed()
+
+while True:
   random.shuffle(x)
   (p1, s1, f1) = random.choice(operators)
   (p2, s2, f2) = random.choice(operators)
@@ -43,26 +54,15 @@ while not done:
       ans = f1(x[0], f2(x[1], x[2]))
     else:
       ans = f2(f1(x[0], x[1]), x[2])
+
+    print "\n", s1.format(x[0], s2.format(x[1], x[2]))
+
+    get_answer(ans)
+
   except (ZeroDivisionError, ValueError):
-    continue
-
-  s = s1.format(x[0], s2.format(x[1], x[2]))
-  print
-  print s
-
-  while True:
-    try:
-      user_ans = raw_input("=> ")
-      if user_ans[0] == "?":
-        print int(ans)
-        break
-      elif int(user_ans) == ans:
-        break
-    except EOFError:
-      done = True
-      break
-    except (ValueError, IndexError):
-      pass
+    pass
+  except (EOFError, KeyboardInterrupt):
+    break
 
 
 
