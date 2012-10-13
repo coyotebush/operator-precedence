23cb6114c956801797af13e57f38feb4e64f4165
Corey Ford <coyotebush22@gmail.com>
2012-10-12 21:18:16 -0700

    main loop

--- precedencequiz.py
+++ precedencequiz.py
@@ -1,48 +1,63 @@
 #!/usr/bin/python
+# vim: ts=2 sts=2 sw=2 et
 import random
 
 operators = [
-		#(14, "!{}", lambda x: not x),
-		#(14, "~{}", lambda x: ~x),
-		#(14, "+{}", lambda x: x),
-		#(14, "-{}", lambda x: -x),
-		(13, "{} * {}",  lambda x, y: x * y),
-		(13, "{} / {}",  lambda x, y: x / y),
-		(13, "{} % {}",  lambda x, y: x % y),
-		(12, "{} + {}",  lambda x, y: x + y),
-		(12, "{} - {}",  lambda x, y: x - y),
-		(11, "{} << {}", lambda x, y: x << y),
-		(11, "{} >> {}", lambda x, y: x >> y),
-		(10, "{} < {}",  lambda x, y: x < y),
-		(10, "{} <= {}", lambda x, y: x <= y),
-		(10, "{} > {}",  lambda x, y: x > y),
-		(10, "{} >= {}", lambda x, y: x >= y),
-		( 9, "{} == {}", lambda x, y: x == y),
-		( 9, "{} != {}", lambda x, y: x != y),
-		( 8, "{} & {}",  lambda x, y: x & y),
-		( 7, "{} ^ {}",  lambda x, y: x ^ y),
-		( 6, "{} | {}",  lambda x, y: x | y),
-		( 5, "{} && {}", lambda x, y: bool(x and y)),
-		( 4, "{} || {}", lambda x, y: bool(x or y)),
-		( 1, "{} , {}",  lambda x, y: y),
-		]
+    #(14, "!{}", lambda x: not x),
+    #(14, "~{}", lambda x: ~x),
+    #(14, "+{}", lambda x: x),
+    #(14, "-{}", lambda x: -x),
+    (13, "{} * {}",  lambda x, y: x * y),
+    (13, "{} / {}",  lambda x, y: x / y),
+    (13, "{} % {}",  lambda x, y: x % y),
+    (12, "{} + {}",  lambda x, y: x + y),
+    (12, "{} - {}",  lambda x, y: x - y),
+    (11, "{} << {}", lambda x, y: x << y),
+    (11, "{} >> {}", lambda x, y: x >> y),
+    (10, "{} < {}",  lambda x, y: x < y),
+    (10, "{} <= {}", lambda x, y: x <= y),
+    (10, "{} > {}",  lambda x, y: x > y),
+    (10, "{} >= {}", lambda x, y: x >= y),
+    ( 9, "{} == {}", lambda x, y: x == y),
+    ( 9, "{} != {}", lambda x, y: x != y),
+    ( 8, "{} & {}",  lambda x, y: x & y),
+    ( 7, "{} ^ {}",  lambda x, y: x ^ y),
+    ( 6, "{} | {}",  lambda x, y: x | y),
+    ( 5, "{} && {}", lambda x, y: bool(x and y)),
+    ( 4, "{} || {}", lambda x, y: bool(x or y)),
+    ( 1, "{} , {}",  lambda x, y: y),
+    ]
 
 random.seed()
 
 x = [2, 3, 5]
-random.shuffle(x)
 
-(p1, s1, f1) = random.choice(operators)
-(p2, s2, f2) = random.choice(operators)
+while True:
+  random.shuffle(x)
+  (p1, s1, f1) = random.choice(operators)
+  (p2, s2, f2) = random.choice(operators)
 
-s = s1.format(x[0], s2.format(x[1], x[2]))
-print s
+  try:
+    if p2 > p1:
+      ans = f1(x[0], f2(x[1], x[2]))
+    else:
+      ans = f2(f1(x[0], x[1]), x[2])
+  except ZeroDivisionError:
+    continue
 
-ans1 = f2(f1(x[0], x[1]), x[2])
-ans2 = f1(x[0], f2(x[1], x[2]))
+  s = s1.format(x[0], s2.format(x[1], x[2]))
+  print s
 
-print int(ans1)
-print int(ans2)
+  user_ans = float("inf")
+  try:
+    while user_ans != ans:
+      try:
+        user_ans = int(raw_input("=> "))
+      except ValueError:
+        user_ans = float("inf")
+  except EOFError:
+    break
+
 
 
 
